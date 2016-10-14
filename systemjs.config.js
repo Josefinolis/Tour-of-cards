eb8480a4ff4cad2aeac2d1e4ea9e808a3ad9fa48
Jose Luis Medina (jdt) <joseluis.medina@tinsa.com>
2016-10-14 16:56:14 +0200

    Include bootstrap css

--- systemjs.config.js
+++ systemjs.config.js
@@ -24,6 +24,8 @@
             // other libraries
             'rxjs':                      'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+            'moment': 'vendor/moment/moment.js',
+            'ng2-bootstrap': 'vendor/ng2-bootstrap',
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
@@ -37,7 +39,9 @@
             'angular-in-memory-web-api': {
                 main: './index.js',
                 defaultExtension: 'js'
-            }
+            },
+            'moment': {format: 'cjs'},
+            'ng2-bootstrap': {}
         }
     });
 })(this);
